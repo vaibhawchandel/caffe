ed767418a88aaf901f5bca04945bcea9f7caabcf
Vaibhaw Singh Chandel <chandel.vaibhaw@gmail.com>
2015-05-17 10:19:02 +0530

    code to create leveldb using pair of image paths and flag

---
+++ examples/siamese/convert_siamese_data.cpp
@@ -0,0 +1,260 @@
+// This program converts a set of images to a lmdb/leveldb by storing them
+// as Datum proto buffers.
+// Usage:
+//   convert_imageset [FLAGS] ROOTFOLDER/ LISTFILE DB_NAME
+//
+// where ROOTFOLDER is the root folder that holds all the images, and LISTFILE
+// should be a list of files as well as their labels, in the format as
+//   subfolder1/file1.JPEG 7
+//   ....
+
+#include <gflags/gflags.h>
+#include <glog/logging.h>
+#include <leveldb/db.h>
+#include <leveldb/write_batch.h>
+#include <lmdb.h>
+#include <sys/stat.h>
+
+#include <algorithm>
+#include <fstream>  // NOLINT(readability/streams)
+#include <string>
+#include <utility>
+#include <vector>
+
+#include <opencv2/opencv.hpp>
+#include "caffe/proto/caffe.pb.h"
+// #include "caffe/util/io.hpp"
+#include "caffe/util/rng.hpp"
+
+using namespace caffe;  // NOLINT(build/namespaces)
+using std::pair;
+using std::string;
+
+DEFINE_bool(gray, false,
+    "When this option is on, treat images as grayscale ones");
+DEFINE_bool(shuffle, false,
+    "Randomly shuffle the order of images and their labels");
+DEFINE_string(backend, "lmdb", "The backend for storing the result");
+DEFINE_int32(resize_width, 0, "Width images are resized to");
+DEFINE_int32(resize_height, 0, "Height images are resized to");
+
+
+bool ReadImageToDatum(const string& filename1, const string& filename2, const int label,
+    const int height, const int width, const bool is_color, Datum* datum) {
+  cv::Mat cv_img1;
+  cv::Mat cv_img2;
+  int cv_read_flag = (is_color ? CV_LOAD_IMAGE_COLOR : CV_LOAD_IMAGE_GRAYSCALE);
+
+  cv::Mat cv_img1_origin = cv::imread(filename1, cv_read_flag);
+  cv::Mat cv_img2_origin = cv::imread(filename2, cv_read_flag);
+  if (!cv_img1_origin.data || !cv_img2_origin.data) {
+    LOG(ERROR) << "Could not open or find file " << filename1 << " : " << filename2;
+    return false;
+  }
+  if (height > 0 && width > 0) {
+    cv::resize(cv_img1_origin, cv_img1, cv::Size(width, height));
+    cv::resize(cv_img2_origin, cv_img2, cv::Size(width, height));
+  } else {
+    cv_img1 = cv_img1_origin;
+    cv_img2 = cv_img2_origin;
+  }
+
+  int num_channels = (is_color ? 6 : 2);
+  datum->set_channels(num_channels);
+  datum->set_height(cv_img1.rows);
+  datum->set_width(cv_img1.cols);
+  datum->set_label(label);
+  datum->clear_data();
+  datum->clear_float_data();
+  string* datum_string = datum->mutable_data();
+  if (is_color) {
+    for (int c = 0; c < num_channels/2; ++c) {
+      for (int h = 0; h < cv_img1.rows; ++h) {
+        for (int w = 0; w < cv_img1.cols; ++w) {
+          datum_string->push_back(static_cast<char>(cv_img1.at<cv::Vec3b>(h, w)[c]));
+        }
+      }
+    }
+    for (int c = 0; c < num_channels/2; ++c) {
+      for (int h = 0; h < cv_img2.rows; ++h) {
+        for (int w = 0; w < cv_img2.cols; ++w) {
+          datum_string->push_back(static_cast<char>(cv_img2.at<cv::Vec3b>(h, w)[c]));
+        }
+      }
+    }
+  } else {  // Faster than repeatedly testing is_color for each pixel w/i loop
+    for (int h = 0; h < cv_img1.rows; ++h) {
+      for (int w = 0; w < cv_img1.cols; ++w) {
+        datum_string->push_back(static_cast<char>(cv_img1.at<uchar>(h, w)));
+      }
+    }
+    for (int h = 0; h < cv_img2.rows; ++h) {
+      for (int w = 0; w < cv_img2.cols; ++w) {
+        datum_string->push_back(static_cast<char>(cv_img2.at<uchar>(h, w)));
+      }
+    }
+  }
+  return true;
+}
+
+
+
+int main(int argc, char** argv) {
+  ::google::InitGoogleLogging(argv[0]);
+
+#ifndef GFLAGS_GFLAGS_H_
+  namespace gflags = google;
+#endif
+
+  gflags::SetUsageMessage("Convert a set of images to the leveldb/lmdb\n"
+        "format used as input for Caffe.\n"
+        "Usage:\n"
+        "    convert_imageset [FLAGS] ROOTFOLDER/ LISTFILE DB_NAME\n"
+        "The ImageNet dataset for the training demo is at\n"
+        "    http://www.image-net.org/download-images\n");
+  gflags::ParseCommandLineFlags(&argc, &argv, true);
+
+  if (argc != 4) {
+    gflags::ShowUsageWithFlagsRestrict(argv[0], "tools/convert_imageset");
+    return 1;
+  }
+
+  bool is_color = !FLAGS_gray;
+  std::ifstream infile(argv[2]);
+//  std::vector<std::pair<string, int> > lines;
+  std::vector<std::tuple<string, string, int> > lines;
+  string filename1;
+  string filename2;
+  int label;
+  while (infile >> filename1 >> filename2 >> label) {
+    lines.push_back(std::tuple<string, string, int>(filename1, filename2, label));
+  }
+  if (FLAGS_shuffle) {
+    // randomly shuffle data
+    LOG(INFO) << "Shuffling data";
+    shuffle(lines.begin(), lines.end());
+  }
+  LOG(INFO) << "A total of " << lines.size() << " images.";
+
+  const string& db_backend = FLAGS_backend;
+  const char* db_path = argv[3];
+
+  int resize_height = std::max<int>(0, FLAGS_resize_height);
+  int resize_width = std::max<int>(0, FLAGS_resize_width);
+
+  // Open new db
+  // lmdb
+  MDB_env *mdb_env;
+  MDB_dbi mdb_dbi;
+  MDB_val mdb_key, mdb_data;
+  MDB_txn *mdb_txn;
+  // leveldb
+  leveldb::DB* db;
+  leveldb::Options options;
+  options.error_if_exists = true;
+  options.create_if_missing = true;
+  options.write_buffer_size = 268435456;
+  leveldb::WriteBatch* batch = NULL;
+
+  // Open db
+  if (db_backend == "leveldb") {  // leveldb
+    LOG(INFO) << "Opening leveldb " << db_path;
+    leveldb::Status status = leveldb::DB::Open(
+        options, db_path, &db);
+    CHECK(status.ok()) << "Failed to open leveldb " << db_path
+        << ". Is it already existing?";
+    batch = new leveldb::WriteBatch();
+  } else if (db_backend == "lmdb") {  // lmdb
+    LOG(INFO) << "Opening lmdb " << db_path;
+    CHECK_EQ(mkdir(db_path, 0744), 0)
+        << "mkdir " << db_path << "failed";
+    CHECK_EQ(mdb_env_create(&mdb_env), MDB_SUCCESS) << "mdb_env_create failed";
+    CHECK_EQ(mdb_env_set_mapsize(mdb_env, 1099511627776), MDB_SUCCESS)  // 1TB
+        << "mdb_env_set_mapsize failed";
+    CHECK_EQ(mdb_env_open(mdb_env, db_path, 0, 0664), MDB_SUCCESS)
+        << "mdb_env_open failed";
+    CHECK_EQ(mdb_txn_begin(mdb_env, NULL, 0, &mdb_txn), MDB_SUCCESS)
+        << "mdb_txn_begin failed";
+    CHECK_EQ(mdb_open(mdb_txn, NULL, 0, &mdb_dbi), MDB_SUCCESS)
+        << "mdb_open failed. Does the lmdb already exist? ";
+  } else {
+    LOG(FATAL) << "Unknown db backend " << db_backend;
+  }
+
+  // Storing to db
+  string root_folder(argv[1]);
+  Datum datum;
+  int count = 0;
+  const int kMaxKeyLength = 256;
+  char key_cstr[kMaxKeyLength];
+  int data_size;
+  bool data_size_initialized = false;
+
+  for (int line_id = 0; line_id < lines.size(); ++line_id) {
+    if (!ReadImageToDatum(root_folder + std::get<0>(lines[line_id]),
+        std::get<1>(lines[line_id]), std::get<2>(lines[line_id]), resize_height, resize_width, is_color, &datum)) {
+      continue;
+    }
+    if (!data_size_initialized) {
+      data_size = datum.channels() * datum.height() * datum.width();
+      data_size_initialized = true;
+    } else {
+      const string& data = datum.data();
+      CHECK_EQ(data.size(), data_size) << "Incorrect data field size "
+          << data.size();
+    }
+    // sequential
+    snprintf(key_cstr, kMaxKeyLength, "%08d_%s", line_id,
+        std::get<0>(lines[line_id]).c_str());
+    string value;
+    datum.SerializeToString(&value);
+    string keystr(key_cstr);
+
+    // Put in db
+    if (db_backend == "leveldb") {  // leveldb
+      batch->Put(keystr, value);
+    } else if (db_backend == "lmdb") {  // lmdb
+      mdb_data.mv_size = value.size();
+      mdb_data.mv_data = reinterpret_cast<void*>(&value[0]);
+      mdb_key.mv_size = keystr.size();
+      mdb_key.mv_data = reinterpret_cast<void*>(&keystr[0]);
+      CHECK_EQ(mdb_put(mdb_txn, mdb_dbi, &mdb_key, &mdb_data, 0), MDB_SUCCESS)
+          << "mdb_put failed";
+    } else {
+      LOG(FATAL) << "Unknown db backend " << db_backend;
+    }
+
+    if (++count % 1000 == 0) {
+      // Commit txn
+      if (db_backend == "leveldb") {  // leveldb
+        db->Write(leveldb::WriteOptions(), batch);
+        delete batch;
+        batch = new leveldb::WriteBatch();
+      } else if (db_backend == "lmdb") {  // lmdb
+        CHECK_EQ(mdb_txn_commit(mdb_txn), MDB_SUCCESS)
+            << "mdb_txn_commit failed";
+        CHECK_EQ(mdb_txn_begin(mdb_env, NULL, 0, &mdb_txn), MDB_SUCCESS)
+            << "mdb_txn_begin failed";
+      } else {
+        LOG(FATAL) << "Unknown db backend " << db_backend;
+      }
+      LOG(ERROR) << "Processed " << count << " files.";
+    }
+  }
+  // write the last batch
+  if (count % 1000 != 0) {
+    if (db_backend == "leveldb") {  // leveldb
+      db->Write(leveldb::WriteOptions(), batch);
+      delete batch;
+      delete db;
+    } else if (db_backend == "lmdb") {  // lmdb
+      CHECK_EQ(mdb_txn_commit(mdb_txn), MDB_SUCCESS) << "mdb_txn_commit failed";
+      mdb_close(mdb_env, mdb_dbi);
+      mdb_env_close(mdb_env);
+    } else {
+      LOG(FATAL) << "Unknown db backend " << db_backend;
+    }
+    LOG(ERROR) << "Processed " << count << " files.";
+  }
+  return 0;
+}
